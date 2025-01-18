9187de5307b991672db27683ba45129ab00f33d2
Ofir <ofirregev06@gmail.com>
2025-01-19 00:45:27 +0200

    add defaults test images fix error with default contractor in PaddleManager.py add unitest for Paddle and PaddleManager

--- OCR/PaddleManager.py
+++ OCR/PaddleManager.py
@@ -1,21 +1,17 @@
 from Paddle import Paddle
 
-
-
-
-
 class PaddleManager:
-    def __new__(self, *args, **kwargs):
-        if not hasattr(self, 'instance'):
-            self.instance = super(PaddleManager, self).__new__(self)
-        return self.instance
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(PaddleManager, cls).__new__(cls)
+        return cls.instance
 
-    def __int__(self):
+    def __init__(self):
         self.ocr = Paddle()
 
     def text_from_image(self, img_path, cls=True):
-        result_ocr =  self.ocr.text_from_image(img_path, cls=cls)[0]
-        only_texts = [line[1][0] for line in result_ocr if line[1][1] >= 0.5]
+        result_ocr =  self.ocr.text_from_image(img_path, cls=cls)
+        only_texts = [line[1][0] for line in result_ocr]
         lines = self.list_of_text_to_lines(only_texts)
         return lines
 
@@ -28,17 +24,26 @@ class PaddleManager:
         one_line = ""
         for line in list_of_text:
             # If the first letter of the line is uppercase, it means that it is a new line
-            if str(line[0][0]).isupper():
-                lines.append(one_line)
+            if str(line[0]).isupper():
+                lines.append(one_line.rstrip())
                 one_line = ""
 
             # If the last letter of the string is a period, it means that it is the end of the line
-            for string in line:
-                if string[-1] == '.':
-                    one_line.join(string)
+            words = line.split(' ')
+            for word in words:
+                if word == "":
+                    continue
+
+                if word[-1] == '.':
+                    one_line += word
                     lines.append(one_line)
                     one_line = ""
                     continue
 
-                one_line.join(string).join('')
+                one_line += word + ' '
+
+        # If the last line is not empty, add it to the list
+        if one_line != "":
+            lines.append(one_line.rstrip())
+
         return lines
---
+++ OCR/TestPaddle.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+from OCR.PaddleManager import PaddleManager
+
+class TestPaddleOCR(unittest.TestCase):
+
+    dict_of_answers = {
+        "basic_test_case.png": "123456",
+        "basic_test_case_blur_level_4.png": "123456",
+        "basic_test_case_blur_level_8.png": "123456",
+        "basic_test_case_blur_level_16.png": "123456",
+        "clear_small_text.jpg": "882093",
+        "test_img_num1.jpg": "704670",
+        "test_img_num2.jpg": "320414",
+        "test_img_num2_blur_level_11.png": "320414",
+    }
+
+    def test_singleton(self):
+        paddle_manager1 = PaddleManager()
+        paddle_manager2 = PaddleManager()
+
+        self.assertIs(paddle_manager1, paddle_manager2, "PaddleManager is not a singleton")
+        self.assertIs(paddle_manager1.ocr, paddle_manager2.ocr, "Paddle is not a singleton")
+
+    def test_images(self):
+        paddle_manager = PaddleManager()
+        image_dir = "./test_images"
+        self.assertTrue(os.path.isdir(image_dir), f"Directory {image_dir} does not exist")
+
+        for filename in os.listdir(image_dir):
+            file_path = os.path.join(image_dir, filename)
+
+            if filename.lower().endswith(('.png', '.jpg')):
+                result = paddle_manager.text_from_image(file_path)
+                result = " ".join(result)
+                real_answer = self.dict_of_answers.get(filename, None)
+                self.assertIsNotNone(real_answer, f"Real answer for {filename} is not provided")
+                self.assertIn(real_answer, result, f"Expected: {real_answer}, got: {result}")
+
+
+if __name__ == '__main__':
+    unittest.main()
