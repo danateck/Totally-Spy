124d8801f4eadc1299ac95bde2e9ec1ed64ef3d5
Ofir <ofirregev06@gmail.com>
2025-03-18 23:37:51 +0200

    add typing for variables and return values

--- YOLO8/detect_phone.py
+++ YOLO8/detect_phone.py
@@ -1,7 +1,9 @@
+from numpy import ndarray
 from ultralytics import YOLO
 import logging
 
 type CordsPoint = tuple[int,int,int,int,float]
+type Image = ndarray
 
 def find_largest_phone_from_cords(tuple_cord_points: list[CordsPoint]) -> list[CordsPoint]:
     """Finds the largest array phone (based on bounding box area)."""
@@ -10,14 +12,14 @@ def find_largest_phone_from_cords(tuple_cord_points: list[CordsPoint]) -> list[C
     return [max(tuple_cord_points, key=lambda box: (box[2] - box[0]) * (box[3] - box[1]))]
 
 
-def sort_by_confidence(tuple_cord_points: list[CordsPoint]):
+def sort_by_confidence(tuple_cord_points: list[CordsPoint]) -> list[CordsPoint]:
     """Sorting the array based on a dynamic confidence threshold."""
     if len(tuple_cord_points) == 0:
         return []
     return sorted(tuple_cord_points, key=lambda box: box[4])
 
 
-def get_phones_from_cords(image, tuple_cord_points: list[CordsPoint]):
+def get_phones_from_cords(image: Image, tuple_cord_points: list[CordsPoint]) -> list[Image]:
     """Crops detected phones from an image based on bounding box coordinates."""
     if len(tuple_cord_points) == 0:
         return []
@@ -40,7 +42,7 @@ class DetectPhone:
         self.model = YOLO(model_path)
         logging.getLogger("ultralytics").setLevel(logging.WARNING)
 
-    def find_cord_for_phones(self, image) -> list[CordsPoint]:
+    def find_cord_for_phones(self, image: Image) -> list[CordsPoint]:
         """Detect phones in an image and return their bounding boxes and confidence scores."""
         results = self.model(image, verbose=False)
         phones = []
