3244c0dd756da2f19f519bfeb65c8a924a34b22c
Ofir <ofirregev06@gmail.com>
2025-03-18 23:31:24 +0200

    add typing for variables and return values

--- YOLO8/detect_phone.py
+++ YOLO8/detect_phone.py
@@ -1,26 +1,28 @@
 from ultralytics import YOLO
 import logging
 
-def find_largest_phone_from_cords(list_cord_points):
-    """Finds the largest detected phone (based on bounding box area)."""
-    if len(list_cord_points) == 0:
+type CordsPoint = tuple[int,int,int,int,float]
+
+def find_largest_phone_from_cords(tuple_cord_points: list[CordsPoint]) -> list[CordsPoint]:
+    """Finds the largest array phone (based on bounding box area)."""
+    if len(tuple_cord_points) == 0:
         return []
-    return [max(list_cord_points, key=lambda box: (box[2] - box[0]) * (box[3] - box[1]))]
+    return [max(tuple_cord_points, key=lambda box: (box[2] - box[0]) * (box[3] - box[1]))]
 
 
-def filter_by_confidence(list_cord_points):
-    """Filters detections based on a dynamic confidence threshold."""
-    if len(list_cord_points) == 0:
+def sort_by_confidence(tuple_cord_points: list[CordsPoint]):
+    """Sorting the array based on a dynamic confidence threshold."""
+    if len(tuple_cord_points) == 0:
         return []
-    return sorted(list_cord_points, key=lambda box: box[4])
+    return sorted(tuple_cord_points, key=lambda box: box[4])
 
 
-def get_phones_from_cords(image, list_cord_points):
+def get_phones_from_cords(image, tuple_cord_points: list[CordsPoint]):
     """Crops detected phones from an image based on bounding box coordinates."""
-    if len(list_cord_points) == 0:
+    if len(tuple_cord_points) == 0:
         return []
     cropped_images = []
-    for x1, y1, x2, y2, _ in list_cord_points:
+    for x1, y1, x2, y2, _ in tuple_cord_points:
         cropped = image[y1:y2, x1:x2]
 
         # Ensure the crop is valid before adding
@@ -38,7 +40,7 @@ class DetectPhone:
         self.model = YOLO(model_path)
         logging.getLogger("ultralytics").setLevel(logging.WARNING)
 
-    def find_cord_for_phones(self, image):
+    def find_cord_for_phones(self, image) -> list[CordsPoint]:
         """Detect phones in an image and return their bounding boxes and confidence scores."""
         results = self.model(image, verbose=False)
         phones = []
