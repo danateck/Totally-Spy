668e423392fc9ca2fcd54cf13b43c4d3932dfa86
Agam Medina <medinaagam@gmail.com>
2025-03-27 19:57:44 +0200

    implemented the changes you told me

--- Enhance Image/pictureChange.py
+++ Enhance Image/pictureChange.py
@@ -1,47 +1,23 @@
 import cv2
-import tkinter as tk
-from tkinter import filedialog
+from numpy import ndarray
 
 
-def enhance_image():
+
+def enhance_image(image) -> ndarray| None:
     """
     Enhances text clarity in an image by applying contrast enhancement,
     noise reduction, sharpening, and binarization while making text slightly thinner.
     """
-    image=loadPicture()
     image_resized=resizePicture(image)
     enhanced_gray=grayScale(image_resized)
     # binary=reduceNoise(enhanced_gray)
     # binary is optional, sometimes results are better without this functionality
 
-    cv2.imshow("Original Image", image_resized)
-    cv2.imshow("Enhanced Image", enhanced_gray)
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    return image
+    return enhanced_gray
 
-def loadPicture():
-    # load picture from computer
-    root = tk.Tk()
-    root.withdraw()  # Hide the root window
-    image_path = filedialog.askopenfilename(
-        title="Select an Image",
-        filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp;*.tif;*.tiff")],
-    )
-
-    if not image_path:
-        print("No image selected.")
-        return
-
-    # Load the image
-    image = cv2.imread(image_path)
-    if image is None:
-        print("Error: Could not load the image.")
-        return
-    return image
-
-def resizePicture(image):
+def resizePicture(image) -> ndarray | None:
     # Resize image while maintaining aspect ratio
     max_width, max_height = 1000, 400
     h, w = image.shape[:2]
@@ -50,7 +26,7 @@ def resizePicture(image):
     image_resized = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_CUBIC)
     return image_resized
 
-def grayScale(image_resized):
+def grayScale(image_resized) -> ndarray | None :
     # Convert to grayscale
     gray = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
 
@@ -59,7 +35,7 @@ def grayScale(image_resized):
     enhanced_gray = clahe.apply(gray)
     return enhanced_gray
 
-def reduceNoise(enhanced_gray):
+def reduceNoise(enhanced_gray) -> ndarray | None:
     # Apply Gaussian blur to reduce noise before binarization
     denoised = cv2.GaussianBlur(enhanced_gray, (3, 3), 0)
     # Apply fixed thresholding with a lower threshold value
@@ -68,5 +44,3 @@ def reduceNoise(enhanced_gray):
 
 
 
-# Run the function
-enhance_image()
