fbc83f2045d1b299a6a4cf555d49e89effa3e9b4
Agam Medina <medinaagam@gmail.com>
2025-03-28 19:35:35 +0300

    implemented the changes you told me

--- Enhance Image/pictureChange.py
+++ Enhance Image/pictureChange.py
@@ -3,7 +3,7 @@ from numpy import ndarray
 
 
 
-def enhance_image(image) -> ndarray| None:
+def enhance_image(image) -> ndarray:
     """
     Enhances text clarity in an image by applying contrast enhancement,
     noise reduction, sharpening, and binarization while making text slightly thinner.
@@ -17,7 +17,7 @@ def enhance_image(image) -> ndarray| None:
     cv2.destroyAllWindows()
     return enhanced_gray
 
-def resizePicture(image) -> ndarray | None:
+def resizePicture(image) -> ndarray:
     # Resize image while maintaining aspect ratio
     max_width, max_height = 1000, 400
     h, w = image.shape[:2]
@@ -26,7 +26,7 @@ def resizePicture(image) -> ndarray | None:
     image_resized = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_CUBIC)
     return image_resized
 
-def grayScale(image_resized) -> ndarray | None :
+def grayScale(image_resized) -> ndarray:
     # Convert to grayscale
     gray = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
 
@@ -35,7 +35,7 @@ def grayScale(image_resized) -> ndarray | None :
     enhanced_gray = clahe.apply(gray)
     return enhanced_gray
 
-def reduceNoise(enhanced_gray) -> ndarray | None:
+def reduceNoise(enhanced_gray) -> ndarray:
     # Apply Gaussian blur to reduce noise before binarization
     denoised = cv2.GaussianBlur(enhanced_gray, (3, 3), 0)
     # Apply fixed thresholding with a lower threshold value
