4c52f42f6ba17c64ac9e27b03d62a54f2fac7b04
emilyluskin <emilyluskinn@gmail.com>
2025-05-05 15:42:25 +0300

    Add domain detection pattern to classify_text function

--- Server/Data_recognition/data_type_recognition.py
+++ Server/Data_recognition/data_type_recognition.py
@@ -2,13 +2,15 @@ import re
 
 
 patterns = {
-    "OTP": r"\b\d{5,6}\b",  # OTP can be 5 or 6 digits
+    "OTP": r"(?:(?:code|otp|one-time|passcode)[^\d]{0,10})?(\d{5,6})(?:[^\d]{0,10}(?:code|otp|one-time|passcode))?"
+,  #smart OTP catch -  5 or 6 digits
     "CREDIT_CARD": r"\b(?:\d{4}[- ]?){3}\d{4}\b",  # 16-digit CC, supports space or - separators
     "PHONE_NUMBER": r"\b\d{10}\b",  # Strictly 10-digit phone number
     "EMAIL": r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b",  # Email
     "PASSWORD": r"(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}",  # Password with special char, min 8 chars
     "ID": r"\b\d{9}\b",  # Strictly 9-digit ID
     "DATE": r"\b\d{1,2}[-/.]\d{1,2}[-/.]\d{2,4}\b",  # Date format (e.g., 25/03/2023)
+    "DOMAIN": r"\b(?:https?://)?(?:www\.)?([\w\-]+\.[a-z]{2,}(?:\.[a-z]{2,})?)", #domain, url
 }
 
 def classify_text(text: str)-> list[tuple[str, str]]:
