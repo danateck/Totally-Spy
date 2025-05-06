52bb6e99dea0be10d6a6497ffc1b51ec52089a37
Dana <dana.gabay8@gmail.com>
2025-05-06 13:03:33 +0300

    deleting client and server

--- update.sh
+++
@@ -1,6 +0,0 @@
-pkill -f "uvicorn server:app --port 4000"
-git pull
-cd Client
-npm install
-npm run build
-cd ~/totally_spy/Totally-Spy/Server/ && source ~/totally_spy/Totally-Spy/.venv/bin/activate && nohup uvicorn server:app --port 4000 --host 0.0.0.0 &
