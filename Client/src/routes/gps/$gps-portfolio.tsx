3f440e588f8e07efa609ee5a0b995045fc261552
Yael Hacshor <yaelhacshor@gmail.com>
2025-06-16 20:56:12 +0300

    update

--- Client/src/routes/gps/$gps-portfolio.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/gps/$gps-portfolio')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/gps/$gps-portfolio"!</div>
-}
